e4f8b015a3ce3ee2165af109e4a8441c4a1b7e55
Ilya Atamas <atamas1llya@gmail.com>
2017-12-17 14:25:45 +0200

    Add login with google

--- src/scenes/Login/components/AuthForm/index.js
+++ src/scenes/Login/components/AuthForm/index.js
@@ -39,8 +39,10 @@ export default class AuthForm extends Component {
 
         <div className="login-social">
           <div>
-            <a href="http://127.0.0.1:8081/api/login/facebook">
-              <Icon.Facebook />
+            <a href="http://127.0.0.1:8081/api/login/google">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#353535" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M21.8,10h-2.6l0,0H12v4h5.7c-0.8,2.3-3,4-5.7,4c-3.3,0-6-2.7-6-6s2.7-6,6-6c1.7,0,3.2,0.7,4.2,1.8l2.8-2.8C17.3,3.1,14.8,2,12,2C6.5,2,2,6.5,2,12s4.5,10,10,10s10-4.5,10-10C22,11.3,21.9,10.6,21.8,10z" />
+              </svg>
             </a>
           </div>
           <div>
@@ -48,8 +50,10 @@ export default class AuthForm extends Component {
               <Icon.Twitter />
             </a>
           </div>
-          <div className="active">
-            <Icon.Mail />
+          <div>
+            <a href="http://127.0.0.1:8081/api/login/facebook">
+              <Icon.Facebook />
+            </a>
           </div>
         </div>
       </div>
